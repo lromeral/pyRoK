2c0d26a3c998d6c2b387a4865a94e0db426a6b03
Luis Romeral <luis.romeral@gmail.com>
2023-09-24 10:48:50 +0200

    ..

--- reporte_final.py
+++ reporte_final.py
@@ -4,14 +4,20 @@ import os
 dir_in = "./reporte_in"
 dir_out = "./reporte_out"
 
-cont = 0
-writer=  pd.ExcelWriter("reporte.xlsx")
-for file in os.listdir(dir_in):
-    print (file)
-    file_path = os.path.join(dir_in,file)
-    print (file_path)
-    csvFrame = pd.read_csv(file_path)
-    print ("hola")
-    csvFrame.to_excel(excel_writer=writer,sheet_name=str(file),index=False)
-    cont+=1
-writer.close()
+
+def get_reino_from_filename(filename:str)->str:
+    result = filename[:filename.find("_")]
+    return result
+
+def generate_report():
+    writer=  pd.ExcelWriter("reporte.xlsx")
+    for file in os.listdir(dir_in):
+        print (file)
+        file_path = os.path.join(dir_in,file)
+        print (file_path)
+        csvFrame = pd.read_csv(file_path)
+        csvFrame.to_excel(excel_writer=writer,sheet_name=get_reino_from_filename(file),index=False)
+    writer.close()
+
+
+generate_report()
