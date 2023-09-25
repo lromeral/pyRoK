9a1b03c55384e29f55703bf44c68b0b042fef033
Luis Romeral <luis.romeral@gmail.com>
2023-09-25 10:16:41 +0200

    reestructuracion

--- dbconnection.py
+++ dbconnection.py
@@ -1,34 +1,47 @@
 # Module Imports
 import mariadb
 import sys
+import os
 from datetime import datetime
 import csv
 from datos_reporte import datos_reporte
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="rok",
-        password="rok#12345.",
-        host="192.168.10.112",
-        port=3306,
-        database="rok"
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
 
+
+
+
+class conn_bd:
+    def __init__(self, dbuser:str, dbpass:str, dbhost:str, dbport:int,dbname:str) -> None:
+    # Connect to MariaDB Platform
+        try:
+            self.conn = mariadb.connect(
+                user=dbuser,
+                password=dbpass,
+                host=dbhost,
+                port=dbport,
+                database=dbname
+        )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+    def exec
 def get_reino_from_filename(filename:str)->str:
     result = filename[:filename.find("_")]
     return result
 
 #origen_filename = '3135_20230912_185502_data.csv'
-origen_filename = '3131_20230922_205830_data.csv'
+origen_filename = './reports_ok/3131_20230924_184114_data.csv'
 
-datetime_string = origen_filename[origen_filename.find("_")+1:origen_filename.find("d")-1]
+for f in os.listdir():
+    if os.path.isfile(f):
+
+
+filename = os.path.basename(origen_filename)
+filename = os.path.splitext(filename)[0]
+datetime_string = filename[filename.find("_")+1:filename.find("d")-1]
 print (datetime_string)
 idscan = 0
 cur = conn.cursor()
-kd = get_reino_from_filename(origen_filename)
+kd = get_reino_from_filename(filename)
 
 
 timestamp = datetime.strptime(datetime_string,'%Y%m%d_%H%M%S')
